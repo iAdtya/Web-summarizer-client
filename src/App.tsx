0e5a8314a5378d6c1711f5167a632a5528659aa3
Aditya Khedekar <adityakhedekar98906@gmail.com>
2024-01-13 13:51:31 +0530

    Add Tailwind CSS CDN and update UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import SummarizeWebpage from "./components/summarize_webpage";
 import Footer from "./components/footer";
 
 function App() {
-
   return (
     <div className="App flex flex-col min-h-screen absolute inset-0 h-full w-full bg-slate-900 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] ">
       <Navbar />
